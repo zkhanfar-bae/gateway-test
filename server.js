7a2ae198db46a8ecb4c7608a493af87877e5259d
zkhanfar-bae <z.khanfar@bankaletihad.com>
2026-05-04 15:18:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
